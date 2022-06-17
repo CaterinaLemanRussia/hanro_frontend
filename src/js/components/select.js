4694d47f12e5ede38da9fd1ca4ab33e33e37bf45
Иван <unbsold@yandex.ru>
2022-06-17 13:24:17 +0300

    Add pages Catalog, Favorites

---
+++ src/js/components/select.js
@@ -0,0 +1,15 @@
+import 'select2';
+
+$(document).ready(function() {
+	const $selectEl = $('.select')
+	if ($selectEl.length) {
+		$selectEl.select2({
+			minimumResultsForSearch: -1,
+			width: '100%'
+		});
+		$('.select2-selection__arrow').find('b').append(
+			'<svg><use xlink:href="/images/sprites.svg#chevron_down"></use></svg>'
+		)
+	}
+
+});
